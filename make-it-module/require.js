f2479e2795c235b289ee82a5227febbecaf1b94b
Nick <nbjenvey@gmail.com>
2019-06-03 19:28:44 +0000

    Printed by making it a module

---
+++ make-it-module/require.js
@@ -0,0 +1,12 @@
+let getHTML = require('./http-functions');
+
+let requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
